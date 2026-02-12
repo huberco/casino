1f6b8328913eabed1c8660d3a3fb1aa1d1b4aee3
huberco <52518044+huberco@users.noreply.github.com>
2026-02-13 03:49:13 +0900

    Update Banner.tsx

--- src/components/common/Banner/Banner.tsx
+++ src/components/common/Banner/Banner.tsx
@@ -9,6 +9,7 @@ import React from "react";
 import { StyledBanner } from "./StyledBanner";
 import SocialMediaButton from "@/components/Buttons/SocialMediaButton/SocialMediaButton";
 import { Button, Image } from "@heroui/react";
+import Link from "next/link";
 
 const Banner = () => {
   return (
@@ -16,10 +17,12 @@ const Banner = () => {
       <div className="main-content w-full @xl:w-auto">
         <Image src={`/assets/LOGO Gamblify/Full LOGO.png`} alt="logo" style={{ maxHeight: "40px" }} />
         <p className="main-heading">
-          Experience true innovation with the highest rewards program within the
-          industry.
+          By default, you will be credited with 10 USDT to your account. This is for you to test the platform and get familiar with the interface.
+          You can deposit more USDT to your account to start playing.
         </p>
-      <Button className="primary-button">Register</Button>
+        <Link href="/account/wallet">
+          <Button className="primary-button">Deposit</Button>
+        </Link>
 
         {/* <RegisterModal buttonText="REGISTER NOW" modalOption="register" /> */}
 
